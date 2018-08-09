443db175b90718aca16dc0c2461ddcf4b2433ad4
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-08-09 13:45:54 +0300

    deploy initial website

---
+++ scripts/provision.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# check if curl is installed
+# install curl if not installed
+which curl || {
+  echo no, curl its not installed 
+  apt-get update
+  apt-get install -y curl
+}
+
+# check if nginx is installed
+# install nginx if not installed
+which nginx || {
+  echo no, nginx its not installed 
+  apt-get update
+  apt-get install -y nginx
+}
+
+# start nginx service
+service nginx start
+
+# deploy our zillion dollar website
+mkdir -p /var/www/html/
+cp /vagrant/index.html /var/www/html/index.html
