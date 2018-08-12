fe4340cca988dea70dba53c8ac21bccba336c024
Alvaro Miranda Aguilera <kikitux@gmail.com>
2018-08-12 12:55:02 +0300

    tidy up - work again

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# make apt to install just packages
 mkdir -p /etc/dpkg/dpkg.cfg.d
 cat >/etc/dpkg/dpkg.cfg.d/01_nodoc <<EOF
 path-exclude /usr/share/doc/*
@@ -11,14 +12,12 @@ path-exclude /usr/share/lintian/*
 path-exclude /usr/share/linda/*
 EOF
 
+# make apt non-interactive and less verbose
 export DEBIAN_FRONTEND=noninteractive
 export APTARGS="-qq -o=Dpkg::Use-Pty=0"
 
 # check if curl is installed
-# install curl if not installed
 # check if nginx is installed
-# install nginx if not installed
-
 which curl nginx || {
   apt-get update ${APTARGS}
   apt-get install -y curl nginx ${APTARGS} 2>/dev/null
